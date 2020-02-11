763f57bd458c176b92e986498a4996f5e1c81bb8
LiJia <lijia@aifantai.com>
2020-02-11 14:29:58 +0800

    add index

---
+++ index/links.js
@@ -0,0 +1,70 @@
+let navs =
+[
+  {
+    "text": "抽屉",
+    "url": "https://m.chouti.com/all/hot",
+    "icon": "https://m.chouti.com/static/image/favicon.png"
+  },
+  {
+    "text": "百度",
+    "url": "https://www.baidu.com",
+    "icon": "https://www.baidu.com/favicon.ico"
+  },
+  {
+    "text": "StackOverflow",
+    "url": "https://stackoverflow.com/",
+    "proxy": true,
+    "icon": "https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico?v=4f32ecc8f43d"
+  },
+  {
+    "text": "OSChina",
+    "url": "https://m.oschina.net",
+    "icon": "https://m.oschina.net/favicon.ico"
+  },
+  {
+    "text": "CnBeta",
+    "url": "https://m.cnbeta.com",
+    "icon": "https://m.cnbeta.com/favicon.ico"
+  },
+  {
+    "text": "优书网",
+    "url": "https://www.yousuu.com",
+    "icon": "https://www.yousuu.com/favicon.ico"
+  },
+  {
+    "text": "IT之家",
+    "url": "https://m.ithome.com",
+    "icon": "https://m.ithome.com/favicon.ico"
+  },
+  {
+    "text": "果壳",
+    "url": "https://m.guokr.com",
+    "icon": "https://m.guokr.com/favicon.ico"
+  },
+  {
+    "text": "太平洋电脑",
+    "url": "https://g.pconline.com.cn",
+    "icon": "https://g.pconline.com.cn/favicon.ico"
+  },
+  {
+    "text": "Chiphell",
+    "url": "https://www.chiphell.com/forum.php?mod=forumdisplay&fid=297&page=1&mobile=2",
+    "icon": "https://www.chiphell.com/favicon.ico"
+  },
+  {
+    "text": "涨姿势",
+    "url": "https://www.zhangzishi.cc",
+    "icon": "https://cdn.zhangzishi.cc/favicon.ico"
+  },
+  {
+    "text": "爱范儿",
+    "url": "https://www.ifanr.com",
+    "icon": "https://images.ifanr.cn/wp-content/themes/ifanr-5.0-pc/static/images/favicon.ico"
+  },
+  {
+    "text": "少数派",
+    "url": "https://www.sspai.com",
+    "icon": "https://cdn.sspai.com/sspai/assets/img/favicon/icon.ico"
+  }
+]
+;
